2d124f33c90ea9a7ce5858e5bd40c776b0677550
Soeren Wegener <wegener92@gmail.com>
2016-12-27 19:54:57 +0100

    Customized i3 colors

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -12,7 +12,11 @@ scrot $IMAGE
 #convert -scale 50% -colors 8 -blur 5x3 -scale 200% $IMAGE $IMAGE
 #convert -scale 50% -colors 8 -blur 5x3 -scale 200% $IMAGE $IMAGE
 #convert -scale 20% -grayscale rec601luma -scale 500% $IMAGE $IMAGE
+#FORTUNE=$(echo "sowas")
 convert -colors 4 -blur 5x3 -scale 20% -scale 500% $IMAGE $IMAGE
+#FORTUNE=$(fortune fortunes)
+#FORTUNE=$(echo "echo \"Manfred\" > /dev/cloud")
+#convert -gravity center -background "rgb(0, 0, 0)" -fill white -size 1920x1080 caption:"$FORTUNE" $IMAGE
 echo 1 > /sys/class/backlight/intel_backlight/brightness
 i3lock -n -u -i $IMAGE
 echo $BRIGHTNESS > /sys/class/backlight/intel_backlight/brightness
