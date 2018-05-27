fe0acfb26774540a1187e12db112fd6061c54ac8
Soeren Wegener <wegener92@gmail.com>
2018-05-27 17:43:48 +0200

    Link font files and clear font cache when executing `link_files.sh`

--- link_files.sh
+++ link_files.sh
@@ -3,3 +3,5 @@ ln -s `pwd`/.vimrc $HOME/.vimrc
 ln -s `pwd`/.gvimrc $HOME/.gvimrc
 ln -s `pwd`/.zshrc $HOME/.zshrc
 ln -s `pwd`/.gitconfig $HOME/.gitconfig
+ln -s `pwd`/powerlevel-fonts/*.ttf $HOME/.local/share/fonts/
+fc-cache -fv
