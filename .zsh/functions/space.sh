ea50c16432284f79c05e236e4ed18b5094c0a5ea
Soeren Wegener <wegener92@gmail.com>
2023-04-16 16:50:03 +0200

    Fix MQTT device name of flipdot elektrowerkstatt bulbs

--- .zsh/functions/space.sh
+++ .zsh/functions/space.sh
@@ -2,10 +2,13 @@ function space {
     mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/hofseite/decke_1/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
     mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/hofseite/decke_2/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
     mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/hofseite/decke_3/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
-    mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/elektroecke/decke_1/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
-    mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/elektroecke/decke_2/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
-    mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/elektroecke/decke_3/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
     mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/mitte/decke_1/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
     mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/mitte/decke_2/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
     mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/mitte/decke_3/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
+}
+
+function space_elektro {
+    mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/elektro_ecke/decke_1/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
+    mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/elektro_ecke/decke_2/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
+    mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/elektro_ecke/decke_3/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
 }
