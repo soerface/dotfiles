a5218b79dffa1284b64ffdb37dbb09846e372a0f
Soeren Wegener <wegener92@gmail.com>
2023-04-17 00:13:21 +0200

    Define GOBIN

--- .zsh/env.sh
+++ .zsh/env.sh
@@ -2,6 +2,7 @@ export ANSIBLE_NOCOWS=1
 # This is useful if we want to intercept by pythons requests library
 # https://github.com/mitmproxy/mitmproxy/issues/2547#issuecomment-399778481
 export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+export GOBIN=$HOME/.local/bin
 export PATH=$(cat <<EOF
 $HOME/.pyenv/bin:
 $HOME/.pyenv/shims:
