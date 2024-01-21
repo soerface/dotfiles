b1c320405d6003b87efa69d104e8fa00140c4202
Soeren Wegener <wegener92@gmail.com>
2024-01-21 23:05:06 +0100

    Update space light

--- .zsh/functions/space.sh
+++ .zsh/functions/space.sh
@@ -4,19 +4,25 @@ function space {
         mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/sofaecke/decke_2/set" -m "{\"state\": \"OFF\"}"
         mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/sofaecke/decke_3/set" -m "{\"state\": \"OFF\"}"
         mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/sitzecke/decke_1/set" -m "{\"state\": \"OFF\"}"
+        mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/sitzecke/decke_2/set" -m "{\"state\": \"OFF\"}"
         mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/trinkregal/n_1/set" -m "{\"state\": \"OFF\"}"
         mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/trinkregal/n_2/set" -m "{\"state\": \"OFF\"}"
         mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/elektro_ecke/decke_1/set" -m "{\"state\": \"OFF\"}"
         mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/elektro_ecke/decke_2/set" -m "{\"state\": \"OFF\"}"
+        mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/spind/set" -m "{\"state\": \"OFF\"}"
+        mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/3ddruck/wand/set" -m "{\"state\": \"OFF\"}"
     else
         mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/sofaecke/decke_1/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
         mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/sofaecke/decke_2/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
         mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/sofaecke/decke_3/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
         mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/sitzecke/decke_1/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
+        mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/sitzecke/decke_2/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
         mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/trinkregal/n_1/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
         mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/trinkregal/n_2/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
         mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/elektro_ecke/decke_1/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
         mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/elektro_ecke/decke_2/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
+        mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/spind/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
+        mosquitto_pub -h 192.168.3.145 -t "zigbee2mqtt/3ddruck/wand/set" -m "{\"state\": \"ON\", \"brightness\": $1}"
     fi
 }
 
